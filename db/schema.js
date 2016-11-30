59c6cb4eda7cba2d3ee3545cbec727de7a288421
Kevin Herrera <1herrerake@gmail.com>
2016-11-30 11:52:36 -0500

    adds relational style relationship

--- db/schema.js
+++ db/schema.js
@@ -10,3 +10,24 @@ db.on('error', err => {
 db.once('open', () => {
   console.log('database has been connected!')
 })
+
+var Schema = mongoose.Schema
+var ObjectId = Schema.ObjectId
+
+var AnswerSchema = new Schema({
+  body: String
+})
+
+var QuestionSchema = new Schema({
+  title: String,
+  body: String,
+  answers: [ {type: Schema.ObjectId, ref: "Answers"} ]
+})
+
+var Question = mongoose.model("Question", QuestionSchema)
+var Answer   = mongoose.model("Answer", AnswerSchema)
+
+module.exports = {
+  Question,
+  Answer
+}
