6e96d2882f7b2874c5a550902824b12e88434a10
Kevin Herrera <1herrerake@gmail.com>
2016-12-01 11:31:18 -0500

    adds angular index

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,20 @@
 angular
   .module("stackLab", [
     "ui.router",
-    "ng-resource"
+    "ngResource"
   ])
   .config([
     "$stateProvider",
     Router
+  ])
+  .factory("Question", [
+    "$resource",
+    Question
+  ])
+  .controller("indexCtrl", [
+    "$state",
+    "Question",
+    indexController
   ])
 
   function Router ($stateProvider) {
@@ -14,4 +23,20 @@ angular
         url: "/",
         templateUrl: "/assets/js/ng-views/welcome.html"
       })
+      .state("index", {
+        url: "/questions",
+        templateUrl: "/assets/js/ng-views/index.html",
+        controller: "indexCtrl",
+        controllerAs: "vm"
+      })
+  }
+
+  function Question ($resource) {
+    return $resource("/questions/:title", {}, {
+      update: {method: "PUT"}
+    })
+  }
+
+  function indexController ($state, Question) {
+    this.questions = Question.query()
   }
