ea838f3d86b6be42cecb6af4b445f8a861c43d6e
Kevin Herrera <1herrerake@gmail.com>
2016-12-01 09:33:53 -0500

    angular setup

--- index.js
+++ index.js
@@ -1,10 +1,41 @@
 var express = require("express");
 var app = express();
+var Schema = require("./db/schema.js")
+var bodyParser = require('body-parser')
+
+
+
+var Question = Schema.Question
+var Answer = Schema.Answer
+
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+
 
 app.listen(4000, () => {
   console.log("app listening on port 4000");
 });
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
+
+
+app.get("/questions", (req, res) => {
+  Question.find({}).then(function(questions) {
+    res.json(questions)
+  })
 });
+
+app.get("/questions/:title", (req, res) => {
+  Question.findOne({title: req.params.title}).then(function(question) {
+    res.json(question)
+  })
+})
+
+app.post("/questions/:title", (req, res) => {
+
+})
+
+app.post("/questions", (req, res) => {
+    Question.create(req.body.question).then(function(question) {
+      res.json(req.body)
+    })
+  })
